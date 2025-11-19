cbc5185ff29a94af1942cf6165fa0c9280f3e2e8
Mike Thongvanh <michael@thongvanh.com>
2025-11-18 18:25:42 -0600

    refact: Update package name and application label for consistency in Android configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapp"
+    namespace = "com.thongvanh.taidam_tutor"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.myapp"
+        applicationId = "com.thongvanh.taidam_tutor"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapp
+package com.thongvanh.taidam_tutor
 
 import io.flutter.embedding.android.FlutterActivity
 
